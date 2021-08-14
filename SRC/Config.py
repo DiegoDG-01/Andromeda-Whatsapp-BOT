01a10b4954cd8bf9d0ff11922a3646bc5f5fb7e2
Diego DG <doguezdiego@gmail.com>
2021-08-14 18:54:57 -0500

    Minor changes

--- SRC/Config.py
+++ SRC/Config.py
@@ -1,5 +1,5 @@
-import json
 from os import getcwd
+from json import load
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,7 +11,7 @@ class Config():
         PathUser = getcwd() + '/Data/Config/User.json'
         
         with open(PathUser, 'r') as UserFileConfig:
-            Data = json.load(UserFileConfig)
+            Data = load(UserFileConfig)
             
             User = WebDriverWait(WebDriver, 5).until(EC.presence_of_element_located((By.XPATH, '//span[@title = "{}"]'.format(Data['main']['WhatsappName']))))
             User.click()
