58f17f3735c76fc0361ce3c204b506a7606c22a4
Diego DG <doguezdiego@gmail.com>
2021-08-14 18:51:04 -0500

    limpieza y nombre interno del proyecto

--- SRC/entrypoint.py
+++ SRC/entrypoint.py
@@ -1,13 +1,26 @@
+########################################################
+########################################################
+##                                                    ##
+##                                                    ##
+## I8,        8        ,8I  88888888ba   88888888ba   ##
+## `8b       d8b       d8'  88      "8b  88      "8b  ##
+##  "8,     ,8"8,     ,8"   88      ,8P  88      ,8P  ##
+##   Y8     8P Y8     8P    88aaaaaa8P'  88aaaaaa8P'  ##
+##   `8b   d8' `8b   d8'    88""""""8b,  88""""""8b,  ##
+##    `8a a8'   `8a a8'     88      `8b  88      `8b  ##
+##     `8a8'     `8a8'      88      a8P  88      a8P  ##
+##      `8'       `8'       88888888P"   88888888P"   ##
+##                                                    ##
+##                 Whatsapp Browser Bot               ##
+##                                                    ##
+########################################################
+########################################################
+
 import Bot
 import Config
-from sys import argv
 from os import getcwd
 from selenium.webdriver import Chrome
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 
 
 def InitWebDriver():
@@ -45,52 +58,9 @@ if(__name__ == '__main__'):
         Bot = Bot.Bot()
         Config = Config.Config(WebDriver)
         
-        
         Bot.ReadMessage(WebDriver)
         
         WebDriver.quit()
     except KeyboardInterrupt:
         print('\nInterrupted By Keyboard')
         WebDriver.quit()
-        
-
-# URL = 'https://web.whatsapp.com/'
-# PathSession = getcwd()
-
-# ClassSearchBar = "_13NKt"
-# ClassButton_Search = "_28-cz"
-# ClassButton_Send = "_4sWnG"
-
-# with Chrome(options=_Chrome_options) as WebDriver:
-
-#     WebDriver.maximize_window()
-#     WebDriver.get(URL)
-    
-#     name = "Testing BOT"
-#     msg = "TEST (FOR) #4"
-    
-#     user = WebDriverWait(WebDriver, 5).until(EC.presence_of_element_located((By.XPATH, '//span[@title = "Testing BOT"]')))
-#     # user = WebDriver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-#     user.click()
-    
-#     msg_box = WebDriverWait(WebDriver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div[2]/div/div[1]')))
-    
-#     for i in range(5):
-
-#         # msg_box = WebDriver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[1]')  # /html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[1]
-        
-#         # WebDriverWait(WebDriver, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[1]')))
-#         msg_box.send_keys(msg)
-#         sleep(0.5)
-#         msg_box.send_keys(Keys.ENTER)
-#         # sleep(0.7)
-        
-#         # button = WebDriverWait(WebDriver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, ClassButton_Send)))
-#         # # Dice que no lo encuentra porque efectivamente no puso ningun mensaje
-#         # # button = WebDriver.find_element_by_class_name(ClassButton_Send)
-#         # button.click()
-#         # sleep(0.7)
-        
-#         sleep(1)
-        
-#         print("Mensaje enviado!")
