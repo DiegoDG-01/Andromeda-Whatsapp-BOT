73a910a8cc4be2e0e250e1a2aadd80c49a28511b
Diego DG <doguezdiego@gmail.com>
2022-07-20 23:42:25 -0500

    Feat: Add module to search on google and get image with information

---
+++ SRC/Functions/GSearch.py
@@ -0,0 +1,192 @@
+import Log
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class GSearch:
+
+    # Function to configure the help function and prepare it for use
+    def __init__(self):
+        self.Argument = None
+        self.commandsFile = None
+        self.Communicate = None
+        self.NameModule = "/GSearch"
+        self.SearchBar_Class = 'gLFyf'
+        self.SearchBar_XPath = '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input'
+        self.Google = 'https://www.google.com/'
+
+        self.log = Log.Generate()
+
+    def requirements(self):
+
+        requeriments = {
+            'CommandExecution': "/GSearch",
+            'ExternalModules': [
+                'commandsFile', 'Communicate', 'InterfaceController', 'WebDriver'
+            ],
+        }
+
+        return requeriments
+
+    def set_Communicate(self, Communicate):
+        self.Communicate = Communicate
+
+    def set_commandFile(self, commandsFile):
+        self.commandsFile = commandsFile
+
+    def set_InterfaceController(self, InterfaceControl):
+        try:
+            self.InterfaceControl = InterfaceControl
+        except Exception as error:
+            self.log.Write("Configure.py | InterfaceControl # " + str(error))
+
+    def set_WebDriver(self, WebDriver):
+        self.WebDriver = WebDriver
+
+    # Function to prepare info to argument
+    def __PrepareArgs(self, args):
+        if args[0] in self.commandsFile['Active'][self.NameModule]['Args'][0].keys():
+            self.Argument = args[0]
+            return True
+        else:
+            return False
+
+    # This function is used to initialize the help function
+    def EntryPoint(self, args=None):
+        # if args is empty or None execute default function else execute different function depending on the args
+        if args is None:
+            return self.Default()
+        else:
+            # check if args exist and is a valid argument
+            if self.__PrepareArgs(args):
+                # Execute the function in charge of managing the help function
+                return self.CommandManager()
+            else:
+                return False
+
+    # This function is used to function to management of the help functions and execute the correct function
+    def CommandManager(self):
+
+        if self.Argument == '-d':
+            return self.DescribeCommand()
+        elif self.Argument == '-l':
+            return self.ListArgs()
+        elif self.Argument == '-s':
+            return self.Search()
+        else:
+            return False
+
+    # This function is used to function default or if no argument is given
+    def Default(self):
+        return self.DescribeCommand()
+
+    def DescribeCommand(self):
+        return self.commandsFile['Active'][self.NameModule]['Desc']
+
+    def ListArgs(self):
+
+        List = self.commandsFile['Active'][self.NameModule]['Args'][0]
+
+        ListToMessage = [key + ': ' + List[key] for key in List.keys()]
+
+        return ListToMessage
+
+    # ================== The next functions are used to execute the correct orders depend ==================
+
+    def __readMessage(self):
+        while True:
+            search = self.Communicate.ReadResponse()
+            if search != False:
+                break
+            sleep(0.5)
+
+        return search
+
+    def __CountLinks(self, WebDriver):
+        cout = 1
+        Content = {
+            'Info': [],
+            'Links': [],
+        }
+        Box_Links_Class = 'tF2Cxc'
+
+        for content in WebDriver.find_elements(By.CLASS_NAME, Box_Links_Class):
+            Content['Info'].append('# '+ str(cout) + ' - ' + content.find_element(By.TAG_NAME, 'h3').text)
+            Content['Links'].append(content.find_element(By.TAG_NAME, 'a'))
+            cout += 1
+
+        Content['Info'].append('# exit - to Cancel')
+
+        return Content
+
+    def __Rollback(self):
+        pass
+
+    def Search(self):
+        self.Communicate.WriteMessage(["# Searching in Google", "# What do you want to search?"])
+        self.Communicate.SendMessage()
+        search = self.__readMessage()
+
+        try:
+            # when create new window automatically open the new tab
+            if not self.InterfaceControl.create_new_window(self.Google):
+                self.__Rollback()
+                return ["", "Error # Could not open new tab"]
+            if not self.InterfaceControl.move_next_tab():
+                self.__Rollback()
+                return ["", "Error # Could not move to new tab"]
+        except Exception as error:
+            self.log.Write("GSearch.py | InterfaceControl # " + str(error))
+
+        # Get temporarily the WebDriver to custom managment the interface
+        TempWebDriver = self.InterfaceControl.get_WebDriver()
+
+        try:
+            SearchBar = WebDriverWait(TempWebDriver, 5).until(EC.element_to_be_clickable((By.XPATH, self.SearchBar_XPath)))
+            SearchBar.click()
+            SearchBar.send_keys(search + Keys.ENTER)
+        except Exception as error:
+            self.log.Write("GSearch.py | WebDriver # " + str(error))
+            self.__Rollback()
+            return ["Error # Could not search"]
+
+        links = self.__CountLinks(TempWebDriver)
+
+        self.InterfaceControl.return_to_main_tab()
+
+        sleep(0.1)
+
+        self.Communicate.WriteMessage(links['Info'])
+        self.Communicate.SendMessage()
+
+        page_to_search = self.__readMessage()
+
+        if page_to_search == 'exit':
+            self.InterfaceControl.close_tabs()
+            return ['', '# Search canceled']
+
+        self.Communicate.WriteMessage([' ', 'Please wait while get information...'])
+        self.Communicate.SendMessage()
+
+        self.InterfaceControl.move_next_tab()
+        links['Links'][int(page_to_search) - 1].click()
+        image_name = self.InterfaceControl.take_screenshot()
+        self.InterfaceControl.return_to_main_tab()
+
+        result, message = self.InterfaceControl.send_mediafile(image_name)
+
+        self.InterfaceControl.close_tabs()
+
+        return [message]
+
+
+    def Select(self, args):
+        pass
+
+    def Back(self, args):
+        pass
+
+    def Exit(self, args):
+        pass
--- SRC/InterfaceControl.py
+++ SRC/InterfaceControl.py
@@ -10,10 +10,17 @@
 ###################################
 
 import Log
+import Tabs
+from os import getcwd
 from json import load
+from pathlib import Path
+from random import randint
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+import Libs.Selenium_Screenshot.Screenshot_Clipping as Screenshot_Clipping
+
 
 class Interface:
 
@@ -31,6 +38,15 @@ class Interface:
 
         self.Chat_List_HTML_Class = "zoWT4"
 
+        self.Path_Screenshot = getcwd() + "/Data/WhatsApp/Screenshot/"
+
+        self.Tabs = Tabs.Tabs(self.WebDriver)
+
+        self.Screenshot = Screenshot_Clipping.Screenshot()
+
+    # Only use to managment the interface for customs modules and not for the main module
+    def get_WebDriver(self):
+        return self.WebDriver
 
     def return_to_bot(self):
 
@@ -60,9 +76,127 @@ class Interface:
         except Exception as e:
             self.Log.Write("InterfaceControl.py | error # " + str(e))
 
-
     def minimize(self):
         self.WebDriver.minimize()
 
     def maximize(self):
         self.WebDriver.maximize()
+
+    def take_screenshot(self, Name=None):
+
+        try:
+            if Name is None:
+                alphabet = [letter for letter in range(65, 91)]
+                Name = [chr(i) for i in alphabet if randint(0, 1) == 1]
+
+                Name = "".join(Name)
+                Name = Name + '.png'
+
+            return self.Screenshot.full_Screenshot(self.WebDriver, save_path=self.Path_Screenshot, image_name=Name,
+                                                   multi_images=True)
+        except Exception as e:
+            self.Log.Write("InterfaceControl.py | Screenshot Error # " + str(e))
+            return False
+
+    # This open the menu of whatsapp to send media files under the chat
+    def open_menu(self):
+        menu_xpath = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div'
+        try:
+            WebDriverWait(self.WebDriver, 5).until(EC.element_to_be_clickable((By.XPATH, menu_xpath))).click()
+            return True
+        except Exception as e:
+            self.Log.Write("InterfaceControl.py | Error - Open menu # " + str(e))
+            return False
+
+    def send_mediafile_button(self):
+        button_xpath = '//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div'
+        try:
+            WebDriverWait(self.WebDriver, 5).until(EC.element_to_be_clickable((By.XPATH, button_xpath))).click()
+            return True
+        except Exception as e:
+            self.Log.Write("InterfaceControl.py | Error - Send mediafile_button # " + str(e))
+            return False
+
+    def send_mediafile(self, file):
+
+        # comprobate if the file exist
+        if Path(file).is_file():
+            if Path(file).stat().st_size > 2000000:
+                return self.send_document(route=file)
+            else:
+                return self.send_image(route=file)
+        else:
+            return False, ['File not found']
+
+    # This send image files to the chat in whatsapp
+    def send_image(self, image_name=None, route=None):
+
+        if route is None and image_name is None:
+            return False
+
+        input_xpath = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[1]/button/input'
+
+        if route is None:
+            image = getcwd() + "/Data/WhatsApp/Screenshot/" + image_name
+        else:
+            image = route
+
+        if self.open_menu():
+            try:
+                self.WebDriver.find_element_by_xpath(input_xpath).send_keys(image)
+                self.send_mediafile_button()
+                return True, ['Image sent']
+            except Exception as e:
+                self.Log.Write("InterfaceControl.py | Error Send Image # " + str(e))
+                return False, ['Error sending image']
+        else:
+            return False
+
+    # In development
+    def send_document(self, name=None, route=None):
+
+        if route is None and name is None:
+            return False
+
+        input_xpath = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[4]/button/input'
+
+        if route is None:
+            image = getcwd() + "/Data/WhatsApp/Screenshot/" + name
+        else:
+            image = route
+
+        if self.open_menu():
+            try:
+                self.WebDriver.find_element_by_xpath(input_xpath).send_keys(image)
+                self.send_mediafile_button()
+                return True, ['Document sent']
+            except Exception as e:
+                self.Log.Write("InterfaceControl.py | Error - Open menu # " + str(e))
+                return False, ['Error sending document']
+
+    # To create new windows is necessary pass name to open
+    def create_new_window(self, URL=None):
+        if URL is not None:
+            return self.Tabs.open(URL)
+        else:
+            return False
+
+    def close_tabs(self, ID=None):
+        try:
+            return self.Tabs.close_tab(ID=ID)
+        except Exception as e:
+            self.Log.Write("InterfaceControl.py | Error - Close tabs # " + str(e))
+            return False
+
+    def move_next_tab(self):
+        return self.Tabs.move_tab(self.Tabs.active_tab() + 1)
+
+    def move_previous_tab(self):
+        return self.Tabs.move_tab(self.Tabs.active_tab() - 1)
+
+    def move_specific_tab(self, position):
+        return self.Tabs.move_tab(position)
+
+    def return_to_main_tab(self):
+        # The function if not pass the number of tab use the main tab
+        return self.Tabs.move_tab()
