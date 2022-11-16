7886020fbbf1fa4c50ac766eaa07a243af1a264f
Diego DG <doguezdiego@gmail.com>
2022-11-15 18:11:59 -0600

    Fix: Add library "Selenium_Screenshot" through PIP and delete Local lib

--- SRC/InterfaceControl.py
+++ SRC/InterfaceControl.py
@@ -19,7 +19,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-import Libs.Selenium_Screenshot.Screenshot_Clipping as Screenshot_Clipping
+from Screenshot import Screenshot as Screenshot_Clipping
 
 
 class Interface:
