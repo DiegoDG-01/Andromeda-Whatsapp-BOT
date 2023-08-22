f6f4f7f286737286542fb6b84a07b25bfbc24e6e
Diego DG <doguezdiego@gmail.com>
2023-08-22 11:32:55 -0600

    Refact: Migrate F1 module to POO

--- SRC/Functions/F1.py
+++ SRC/Functions/F1.py
@@ -1,26 +1,17 @@
-import Log
 import requests
-# import DataBase
 from json import load
 from pathlib import Path
 from os import getcwd, environ
+from Functions.Base import BaseModule
 
-class F1:
 
-    # API Documentation:
-    # https://documenter.getpostman.com/view/11586746/SztEa7bL#46c7fbee-e90f-409f-b2ff-d8b77e85e5f6
+class F1(BaseModule):
 
-    # Function to configure the help function and prepare it for use
     def __init__(self):
-        self.Argument = None
-        self.Communicate = None
-        self.commandsFile = None
-        self.ModuleMessages = None
-        self.AdditionalArgs = None
+        super().__init__('f1')
 
-        self.NameModule = "/f1"
-        self.Log = Log.Generate()
-
+        # API Documentation:
+        # https://documenter.getpostman.com/view/11586746/SztEa7bL#46c7fbee-e90f-409f-b2ff-d8b77e85e5f6
         self.API_Url = "http://ergast.com/api/f1/"
 
         PathModuleMessages = str(Path(getcwd() + '/Data/Modules/Messages/F1.json'))
@@ -30,57 +21,7 @@ class F1:
             self.ModuleMessages = self.ModuleMessages[environ['Language']]
             file.close()
 
-    def requirements(self):
-
-        requeriments = {
-            # In CommandExecution is necessary define the name that the user will use to call the command
-            'CommandExecution': "/f1",
-            # In ExternalModules is necessary define the modules necessary to make the module work in the list
-            # >> commandsFile: is a dictionary with the commands and the information of the command
-            # >> Communicate: is an object that will be used to communicate with the user
-            # >> InterfaceController: is an object that will be used to control the web WhatsApp interface
-            # >> Schedule: is an object that will be used to control the schedule of the module
-            'ExternalModules': [
-                'commandsFile', 'Communicate'
-            ],
-        }
-
-        return requeriments
-
-    def set_Communicate(self, Communicate):
-        self.Communicate = Communicate
-
-    def set_commandFile(self, commandsFile):
-        self.commandsFile = commandsFile
-
-    # Function to prepare info to argument
-    def __PrepareArgs(self, args, additionalArgs):
-        if args in self.commandsFile['Active'][self.NameModule]['Args'][0].keys():
-            self.Argument = args
-
-            if additionalArgs is not None:
-                self.AdditionalArgs = additionalArgs
-
-            return True
-        else:
-            return False
-
-    # This function is used to initialize the help function
-    def EntryPoint(self, args=None, additionalArgs=None):
-        # if args is empty or None execute default function else execute different function depending on the args
-        if args is None:
-            return self.Default()
-        else:
-            # check if args exist and is a valid argument
-            if self.__PrepareArgs(args, additionalArgs):
-                # Execute the function in charge of managing the help function
-                return self.CommandManager()
-            else:
-                return False
-
-    # This function is used to function to management of the help functions and execute the correct function
     def CommandManager(self):
-
         if self.Argument == '-d':
             return self.DescribeCommand()
         elif self.Argument == '-l':
@@ -91,23 +32,6 @@ class F1:
             return self.send_request(type='results')
         else:
             return False
-
-    # This function is used to function default or if no argument is given
-    def Default(self):
-        return self.DescribeCommand()
-
-    def DescribeCommand(self):
-        return self.commandsFile['Active'][self.NameModule]['Desc']
-
-    def ListArgs(self):
-
-        List = self.commandsFile['Active'][self.NameModule]['Args'][0]
-
-        ListToMessage = [key + ': ' + List[key] for key in List.keys()]
-
-        return ListToMessage
-
-    # ================== The next functions are used to execute the correct orders depend ==================
 
     def send_request(self, results=3, limit=20, type='qualifying') -> list:
 
